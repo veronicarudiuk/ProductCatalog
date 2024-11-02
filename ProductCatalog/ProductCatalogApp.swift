92b961576809064b5fa4d5c9358e3a668cb2ca87
veronicarudiuk <veronicarudiuk@gmail.com>
2024-11-02 14:14:14 +0400

    Initial Commit

---
+++ ProductCatalog/ProductCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCatalogApp.swift
+//  ProductCatalog
+//
+//  Created by Veronica Rudiuk on 02/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ProductCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
